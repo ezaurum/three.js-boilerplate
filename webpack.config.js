8b2efa5ac09dbd2e81edec6c565630731b260609
조석규 <ezaurum@gmail.com>
2020-04-15 21:34:57 +0900

    feat: Add dev server and hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,7 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   entry: "./src/index.ts",
   devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true,
-  },
   plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Hot Module Replacement',
@@ -28,5 +23,10 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
   },
 }
