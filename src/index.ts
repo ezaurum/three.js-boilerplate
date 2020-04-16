2558cad580d40a1c262eaf3c13da2b8de7f2c16a
조석규 <ezaurum@gmail.com>
2020-04-16 17:05:49 +0900

    feat: Add webgl compatibility check

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {camera, renderer, scene} from "./game/game"
+import {WEBGL} from "three/examples/jsm/WebGL";
 
 document.body.appendChild((() => {
     let htmlDivElement = document.createElement("div");
@@ -13,4 +14,14 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-animate();
+if ( WEBGL.isWebGLAvailable() ) {
+
+    // Initiate function or other initializations here
+    animate();
+
+} else {
+
+    const warning = WEBGL.getWebGLErrorMessage();
+    document.getElementById( 'container' ).appendChild( warning );
+
+}
